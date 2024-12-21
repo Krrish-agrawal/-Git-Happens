32d964c9af2a5bdebabae7d7591a03508273b482
Krrish <agruskrrishofficial@gmail.com>
2024-12-22 00:27:17 +0530

    updated tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,8 +3,22 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          primary: '#1a1a1a',
+          secondary: '#2d2d2d',
+          text: '#ffffff'
+        },
+        light: {
+          primary: '#ffffff',
+          secondary: '#f3f4f6',
+          text: '#333333'
+        }
+      }
+    },
   },
   plugins: [],
 } 
